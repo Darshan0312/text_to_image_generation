dcb26a280fe5d1ead2cf880cfa92c464ab7a4878
Darshan BR <163033878+Darshan0312@users.noreply.github.com>
2025-02-24 23:12:57 +0530

    Update img.py

--- img.py
+++ img.py
@@ -37,7 +37,7 @@ pipe = StableDiffusionXLPipeline.from_pretrained(base, unet=unet, torch_dtype=to
 pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
 
 # Ensure using the same inference steps as the loaded model and CFG set to 0.
-image = pipe("Generate a simple black-and-white line diagram of India's map. The outline should be clear and precise, with smooth curves and sharp edges representing India's borders. The map should not include any labels, colors, or additional details—just a clean, educational line drawing. The focus should be on accuracy and simplicity, making it easy for students to understand and trace.", num_inference_steps=4, guidance_scale=0).images[0]
+image = pipe("Faraday's Law", num_inference_steps=4, guidance_scale=0).images[0]
 image.save("output10.png")
 
 
